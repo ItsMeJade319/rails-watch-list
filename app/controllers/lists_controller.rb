8a0c09d488e1eb0aed7dfd3824787c588ce6272f
agung dharmawan <loaizax745@gmail.com>
2026-05-28 16:37:45 +0800

    watch-list-2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.where(list: @list)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to lists_path, status: :see_other
+  end
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
